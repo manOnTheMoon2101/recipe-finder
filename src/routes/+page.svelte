b924c4f44f2b90818a2bbb232ba00cb21c34a164
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-05-20 20:18:41 +0200

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<Button class="bg-red-400 mx-6">Click me</Button>
+
+
+
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+  </script>
+  
