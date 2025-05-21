8d8443e82ffbfd960ac9d362f68daa892cc97f6c
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-05-21 18:43:14 +0200

    navbar added

---
+++ src/lib/components/Components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from "$app/stores";
+  $: currentPath = $page.url.pathname;
+</script>
+
+<nav
+  style="display: flex; gap: 1rem; padding: 1rem; background-color: #f0f0f0;"
+>  <a href="/" style:font-weight={currentPath === "/" ? "bold" : "normal"}
+    >Home</a
+  >
+  <a
+    href="/about"
+    style:font-weight={currentPath === "/about" ? "bold" : "normal"}>About</a
+  >
+</nav>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	
+	import Navbar from '$lib/components/Components/Navbar.svelte';
 	let { children } = $props();
 </script>
 
+<div>
+	<Navbar />
+</div>
 {@render children()}
