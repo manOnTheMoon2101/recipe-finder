306ba483973e43ea88c12ba75c48491e553ecc37
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-05-21 18:50:43 +0200

    theme toggler added

--- src/lib/components/Components/Navbar.svelte
+++ src/lib/components/Components/Navbar.svelte
@@ -1,16 +1,16 @@
 <script>
   import { page } from "$app/stores";
+  import ThemeToggler from "./Navbar/themeToggler.svelte";
   $: currentPath = $page.url.pathname;
 </script>
 
-<nav
-  style="display: flex; gap: 1rem; padding: 1rem; background-color: #f0f0f0;"
->  <a href="/" style:font-weight={currentPath === "/" ? "bold" : "normal"}
+<nav>
+  <a href="/" style:font-weight={currentPath === "/" ? "bold" : "normal"}
     >Home</a
   >
   <a
     href="/about"
     style:font-weight={currentPath === "/about" ? "bold" : "normal"}>About</a
   >
+  <ThemeToggler />
 </nav>
-
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import Navbar from '$lib/components/Components/Navbar.svelte';
+	import { ModeWatcher } from "mode-watcher";
 	let { children } = $props();
 </script>
 
 <div>
 	<Navbar />
+	<ModeWatcher />
 </div>
+
 {@render children()}
