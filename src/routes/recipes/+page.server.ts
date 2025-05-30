bae7a4d41dc9002e35b9ade13f3055017128592d
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-05-30 20:29:05 +0200

    api start

---
+++ src/routes/recipes/+page.server.ts
@@ -0,0 +1,10 @@
+export const load = async () => {
+	return {
+		recipes: [
+			{ name: 'Alice', age: 30 },
+			{ name: 'Bob', age: 25 },
+			{ name: 'Charlie', age: 28 },
+			{ name: 'Diana', age: 22 }
+		]
+	};
+};
--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,1 +1,12 @@
-<h2>recipes</h2>
+<div>
+{#each recipes as recipe }
+    
+{recipe.name}
+{/each}
+</div>
+
+
+<script lang="ts">
+    export let data;
+    const recipes = data.recipes;
+</script>
